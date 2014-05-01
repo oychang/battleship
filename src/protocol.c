55184e68972ae7f0de251276a76278f675c550e0
Oliver Chang <oliver@oychang.com>
2014-05-01 14:24:32 -0400

    Comment out logging junk in protocol.

--- src/protocol.c
+++ src/protocol.c
@@ -15,10 +15,10 @@ pack_request(char * buf, struct bs_req * request)
         // ensure null-terminated
         request->data.name[MAX_USERNAME_CHARS - 1] = '\0';
         strncpy(&buf[size], request->data.name, MAX_USERNAME_CHARS);
-        printf("Result of packing: %s\n", request->data.name);
+        /*printf("Result of packing: %s\n", request->data.name);
         if (request->data.name[strlen(request->data.name)] == '\0') {
-	  printf("Null-terminating character was correctly packed in\n");
-	}
+      printf("Null-terminating character was correctly packed in\n");
+    }*/
         size += strlen(request->data.name) + 1;
         break;
     case PLACE:
@@ -76,27 +76,27 @@ pack_response(char * buf, struct bs_resp * response)
 enum bs_req_opcode
 parse_request(char * buf, struct bs_req * req)
 {
-    int index;
+    // int index;
     req->opcode = buf[0];
     switch (buf[0]) {
     case NAME:
-      for (index = 1; index <= MAX_USERNAME_CHARS; index++) {
-        req->data.name[index - 1] = buf[index];
-        printf("Goes through for loop, %d\n", (int)(buf[index]));
+      /*for (index = 1; index <= MAX_USERNAME_CHARS; index++) {
+        req->data.name[index - 1] = buf[index];*/
+/*        printf("Goes through for loop, %d\n", (int)(buf[index]));
         if (buf[index] == '\0') {
           printf("reached null character at index: %d\n", index);
           break;
-	}
+    }
       }
       printf("in protocol print: %s\n", req->data.name);
       printf("in prot print supposed 0: %d\n", req->data.name[3]);
       printf("What's in buffer?: %d\n", buf[4]);
-      printf("in protocol print length: %zd\n", strlen(req->data.name));
-      /*
+      printf("in protocol print length: %zd\n", strlen(req->data.name));*/
+
         strncpy(req->data.name, &buf[1], MAX_USERNAME_CHARS);
-        printf("Name being parsed: %s\n", &buf[1]);
-        printf("Name being structed: %s\n", req->data.name);
-      */
+/*        printf("Name being parsed: %s\n", &buf[1]);
+        printf("Name being structed: %s\n", req->data.name);*/
+
         break;
     case PLACE:
         req->data.ship.type = buf[1];
